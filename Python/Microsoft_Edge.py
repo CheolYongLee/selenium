4b22b015414b689fd6cbc125f8a1eaa59797dbe9
이철용 <82740507+CheolYongLee@users.noreply.github.com>
2022-01-28 11:07:21 +0900

    #2 selenium 검색 기능 추가
    네이버 홈 화면에서 검색창에 'facebook'을 입력하고 검색 버튼을 입력하는 기능 추가
    8번 줄) search_input의 send_key 함수에 'facebook' 이외의 원하는 값 입력하여 검색 가능

--- Python/Microsoft_Edge.py
+++ Python/Microsoft_Edge.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 
-# 다운받은 웹 드라이버 경로 지정
-browser = webdriver.Edge('C:/Users/Administrator/selenium/edgedriver/msedgedriver.exe')
 
-# 사이트 주소를 입력하여 웹 페이지 접속
-browser.get("http://www.naver.com")
+browser = webdriver.Edge('C:/Users/Administrator/selenium/edgedriver/msedgedriver.exe') # 다운받은 웹 드라이버 경로 지정
+
+browser.get("http://www.naver.com") # 사이트 주소를 입력하여 웹 페이지 접속
+
+search_input = browser.find_element_by_xpath('//*[@id="query"]').send_keys('facebook') # xpath를 이용하여 네이버 홉의 검색창에 겁색어 입력
+
+search_btn = browser.find_element_by_class_name("btn_submit").click() # class name을 이용하여 네이버 홉의 검색 버튼 클릭
